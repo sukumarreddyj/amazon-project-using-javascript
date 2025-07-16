ef95dc4bbcad742227590bc7c8e1248e9dd35675
sukumarreddyj <reddyjsukumar@gmail.com>
2025-07-16 22:29:44 +0530

    making modules to better arrrange the code

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,32 @@
-const cart = [];
+// This is the cart array that will hold the items added to the cart
+export const cart = [];
+
+// this is the fuction that will add the items to the cart
+// it will check if the item is already in the cart, if it is then it will
+export function addToCart(itemId) {
+     let matchingItem;
+    cart.forEach((product)=>{
+      if(itemId === product.itemId){
+        matchingItem = product;
+      }
+    });
+    
+    if(matchingItem){
+        matchingItem.quantity++;
+      } else {
+        cart.push({
+          itemId: itemId,
+          quantity: 1
+        });
+      }
+    }
+
+
+
+
+
+
+
+
+
+
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,7 @@
+import { dataitems } from '../data/products.js';
+import { cart, addToCart } from '../data/cart.js';
+
+
 // loading the items on to the webpage till now I just loaded the data 
 let productItemsHtml='';
 
@@ -56,32 +60,23 @@ dataitems.forEach((dataitem)=>{
 });
 document.querySelector(".product-items-display").innerHTML= productItemsHtml;
 
+
+// this function will update the cart quantity in the header
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((product) => {
+    cartQuantity += product.quantity;
+  });
+  document.querySelector(".js-cart-quantity").textContent = cartQuantity;
+}
+
+
+// adding the event listener to the add to cart button
+// this will add the item to the cart and update the cart quantity
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener("click",(event)=>{
     const itemId = button.dataset.itemId;
-    let matchingItem;
-    cart.forEach((product)=>{
-      if(itemId === product.itemId){
-        matchingItem = product;
-      }
-    });
-    
-    if(matchingItem){
-        matchingItem.quantity++;
-      } else {
-        cart.push({
-          itemId: itemId,
-          quantity: 1
-        });
-      }
-      let cartQuantity = 0;
-      cart.forEach((product)=>{
-        cartQuantity += product.quantity;
-      });
-      document.querySelector(".js-cart-quantity").textContent = cartQuantity;
-    });
-
+    addToCart(itemId);
+    updateCartQuantity();
   });
-
-
-
+});
