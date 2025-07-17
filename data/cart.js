900bd3e0d57e05596a5f8ee7913f2c75b8018666
sukumarreddyj <reddyjsukumar@gmail.com>
2025-07-17 16:36:09 +0530

    making the html of cart using  js

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,14 @@
 // This is the cart array that will hold the items added to the cart
-export const cart = [];
+export const cart = [
+{
+    itemId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 1
+},
+{    itemId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 2
+}
+
+];
 
 // this is the fuction that will add the items to the cart
 // it will check if the item is already in the cart, if it is then it will
