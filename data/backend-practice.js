f7dbd4b32849f4c2addd8ae2051a0e0239e204eb
sukumarreddyj <reddyjsukumar@gmail.com>
2025-07-20 16:04:44 +0530

    making it link to backend

--- data/backend-practice.js
+++
@@ -1,8 +0,0 @@
-let abc = new XMLHttpRequest();
-abc.addEventListener('load', () => {
-    console.log(abc.response);
-});
-
-abc.open('GET','https://supersimplebackend.dev/documentation');
-abc.send();
-    
